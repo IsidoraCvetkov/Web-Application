055114b666bb5fe74b340694bcd317f033a9c628
IsidoraCvetkov <isidora.cvetkov96@gmail.com>
2023-02-25 21:03:15 +0100

    ApplicationDbContext - DbSet

---
+++ WebApp/WebApp/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using WebApp.Models;
+
+namespace WebApp.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Pozicija> Pozicije { get; set; }
+        public ApplicationDbContext()
+            //Reference name connection string from web.config file 
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
