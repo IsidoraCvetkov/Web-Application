b221899b4f870be4832f74ecef7046645520270d
IsidoraCvetkov <isidora.cvetkov96@gmail.com>
2023-02-25 21:02:29 +0100

    Model tabele pozicija

---
+++ WebApp/WebApp/Models/Pozicija.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Pozicija
+    {
+        [Key]
+        public int IdPozicija { get; set; }
+        public string NazivPozicije { get; set; }
+    }
+}
