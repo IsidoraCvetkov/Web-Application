4d35358889afd7bddeb3d12466388f29bbf011bf
IsidoraCvetkov <isidora.cvetkov96@gmail.com>
2023-02-25 22:41:22 +0100

    Model GodisnjiOdmor tabele, dbSet

---
+++ WebApp/WebApp/Models/GodisnjiOdmor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+    public class GodisnjiOdmor
+    {
+        [Key]
+        public int IdGodisnjiOdmor { get; set; }
+        [ForeignKey("Radnik")]
+        public int RadnikId { get; set; }
+        public Radnik Radnik { get; set; }
+        public int BrojDana { get; set; }
+    }
+}
--- WebApp/WebApp/Persistence/ApplicationDbContext.cs
+++ WebApp/WebApp/Persistence/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace WebApp.Persistence
         public DbSet<Pozicija> Pozicije { get; set; }
         public DbSet<Nadredjen> Nadredjeni { get; set; }
         public DbSet<Radnik> Radnici { get; set; }
+        public DbSet<GodisnjiOdmor> GodisnjiOdmori { get; set; }
         public ApplicationDbContext()
             //Reference name connection string from web.config file 
             : base("DefaultConnection")
