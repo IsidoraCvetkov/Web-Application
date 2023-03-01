f7781b5f36924c7b6f8ac5ba9fc02f040376d2b9
IsidoraCvetkov <isidora.cvetkov96@gmail.com>
2023-03-01 13:28:10 +0100

    Config register

--- WebApp/WebApp/App_Start/WebApiConfig.cs
+++ WebApp/WebApp/App_Start/WebApiConfig.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Security.OAuth;
 using System.Web.Http;
+using Unity;
+using WebApp.App_Start;
 
 namespace WebApp
 {
@@ -9,6 +10,19 @@ namespace WebApp
     {
         public static void Register(HttpConfiguration config)
         {
+            var container = new UnityContainer();
+            var resolver = new UnityResolver(container);
+            resolver.RegisterTypes();
+            config.DependencyResolver = resolver;
+
+            GlobalHost.DependencyResolver = new SignalRUnityDependencyResolver(container);
+
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
