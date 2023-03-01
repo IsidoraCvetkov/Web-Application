d34c1cfef4dd2b30df957e3f7f38bae199c9a699
IsidoraCvetkov <isidora.cvetkov96@gmail.com>
2023-03-01 23:14:11 +0100

    Routing komponenti

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlataComponent } from './plata/plata.component';
+import { PozicijaComponent } from './pozicija/pozicija.component';
 import { RadnikComponent } from './radnik/radnik.component';
 
 
 const routes: Routes = [
   {path:"radnik", component: RadnikComponent},
-
+  {path:"home", component: HomeComponent},
+  {path:"plata", component: PlataComponent},
+  {path:"pozicija", component: PozicijaComponent},
+  
+  {path: "", component: HomeComponent, pathMatch: "full"},
   {path: "**", redirectTo: "home"}
 ];
 
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RadnikComponent } from './radnik/radnik.component';
+import { HomeComponent } from './home/home.component';
+import { PozicijaComponent } from './pozicija/pozicija.component';
+import { PlataComponent } from './plata/plata.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RadnikComponent
+    RadnikComponent,
+    HomeComponent,
+    PozicijaComponent,
+    PlataComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule, //dodala
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
