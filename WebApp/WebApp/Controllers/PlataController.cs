a254b20b768d1cd1d5503c43013faad0b279dc3a
IsidoraCvetkov <isidora.cvetkov96@gmail.com>
2023-03-01 12:34:47 +0100

    Dodavanje novih packages za EF

--- WebApp/WebApp/Controllers/PlataController.cs
+++ WebApp/WebApp/Controllers/PlataController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApp.Models;
 using WebApp.Persistence.UnitOfWork;
 
 namespace WebApp.Controllers
@@ -11,6 +14,24 @@ namespace WebApp.Controllers
         public PlataController()
         {
 
+        }
+        [AllowAnonymous]
+        [Route("GetAllPlatas")]
+        public IEnumerable<Plata> GetLines()
+        {
+            List<Plata> lines = db.Plata.GetAll().ToList();
+            List<Plata> ret = new List<Plata>();
+
+            foreach (Plata l in lines)
+            {
+                Plata lp = new Plata() { IdPlata = l.IdPlata,
+                                         IznosPlate = l.IznosPlate,
+                                         RadnikId = l.RadnikId,
+                                         Pozicija = l.Pozicija,
+                                         DatumPromene = l.DatumPromene };
+                ret.Add(lp);
+            }
+            return ret;
         }
         protected override void Dispose(bool disposing)
         {
