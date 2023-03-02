5ac45df2c3fe5dce7d744bc31fb4d931ebccb94f
IsidoraCvetkov <isidora.cvetkov96@gmail.com>
2023-03-02 22:10:21 +0100

    Komponenta plata i servis sa prikazom svih podataka o platama iz baze

---
+++ angular-app/src/app/plata/plata.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Plata, PlataModel } from '../models/plata';
+import { PlataService } from '../services/plata.service';
+
+@Component({
+  selector: 'app-plata',
+  templateUrl: './plata.component.html',
+  styleUrls: ['./plata.component.css']
+})
+export class PlataComponent implements OnInit {
+  platasAll: Plata[];
+  platas: PlataModel[];
+
+  constructor(private plataService: PlataService) { }
+
+  async ngOnInit() {
+    this.platasAll = await this.plataService.getAllPlatas();
+    this.platas = await this.plataService.getPlatas();
+  }
+}
